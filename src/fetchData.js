e9242ddd5f969414464925a0479ec435254c5c3d
gartler <sebastian.niederle@gmail.com>
2020-05-06 00:20:12 +0200

    edit db.json

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3'
 
 class FetchData extends Component {
   constructor(props) {
    super (props);
 
-   this.stata = {
+   this.state = {
      data: [{timestamp:55, value:44, id:3}],
    };
   }
@@ -19,11 +20,58 @@ class FetchData extends Component {
 
   componentDidMount() {
    this.fetchData()
+   this.initGraph()
   }
+  initGraph() {
+    // const width = 640;
+    // const height = 480;
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+    
+    // append the svg object to the body of the page
+    const svgCanvas = d3.select(".graph")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
+    const data = this.state.data
+// Add X axis --> it is a date format
+var x = d3.scaleLinear()
+.domain(d3.extent(data, function(d) { return d.date; }))
+.range([ 0, width ]);
+svgCanvas.append("g")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x));
+
+// Add Y axis
+var y = d3.scaleLinear()
+.domain([0, d3.max(data, function(d) { return +d.value; })])
+.range([ height, 0 ]);
+svgCanvas.append("g")
+.call(d3.axisLeft(y));
+
+// Add the line
+svgCanvas.append("path")
+.datum(data)
+.attr("fill", "none")
+.attr("stroke", "steelblue")
+.attr("stroke-width", 1.5)
+.attr("d", d3.line()
+  .x(function(d) { return x(d.date) })
+  .y(function(d) { return y(d.value) })
+  )
+
+  }
   render (){
    return(
     <div className="fetch">
+      test
+      <div className="graph"></div>
        {JSON.stringify(this.state.data)}
     </div>
      )
